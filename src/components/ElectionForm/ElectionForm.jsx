74a57729a56fd2dde4b21fdd3f9ac60461a5f1dc
Raniya-thayyil <18gcs22@meaec.edu.in>
2022-04-23 14:13:50 +0530

    form

--- src/components/ElectionForm/ElectionForm.jsx
+++ src/components/ElectionForm/ElectionForm.jsx
@@ -1,10 +1,22 @@
 import Button from '@restart/ui/esm/Button'
 import React from 'react'
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function ElectionForm(props) {
+  const [startDate, setStartDate] = useState(new Date());
   return (
     <div>
-        <input >
+      <label style={{paddingTop:'13px'}}>
+            &nbsp;Candidate:
+          </label>
+        <input id="candidate" className="form-content" type="text"  autocomplete="on" required />
+        <label style={{paddingTop:'13px'}}>
+            &nbsp;Candidate:
+          </label>
+        <input id="candidate" className="form-content" type="text"  autocomplete="on" required />
+        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
       <Button as="input" type="submit" value="Back" className="col-md-12  text-right" onClick={()=>{props.setViewForm(false)}}/>{' '}
     </div>
   )
